8258837d48770558ee64d3855bc81d3941534741
bala-amypo <bala@amypo.in>
2025-12-26 03:51:18 +0000

    User commit

---
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -0,0 +1,21 @@
+package com.example.demo.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+import java.util.Set;
+
+public class JwtTokenProvider {
+
+    public String createToken(Long userId, String email, Set<String> roles) {
+        return "token";
+    }
+
+    public boolean validateToken(String token) {
+        return true;
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.claims().setSubject("user");
+    }
+}
