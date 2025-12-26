2d561fad4dbac5b940662ce6ce150e89cbb8d306
bala-amypo <bala@amypo.in>
2025-12-26 14:31:51 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -11,9 +14,14 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                // API metadata
                 .info(new Info()
                         .title("Visitor Risk Scoring API")
                         .version("1.0")
-                        .description("Backend API documentation using Swagger / OpenAPI"));
+                        .description("Backend API documentation for Visitor Risk Scoring Engine"))
+                // Server configuration
+                .servers(List.of(
+                        new Server().url("https://9038.408procr.amypo.ai/")
+                ));
     }
 }
